8b971eaa959e877e0dc19daac47faeecf5b314d0
ca-pinto <charly.paint02@gmail.com>
2024-05-15 00:18:08 -0600

    Se agregaron funciones a la clase Gestor Colas Ordenamiento de colas, y busqueda de procesos dentro de la cola

--- Grupo3_TaskManager/GestorColas.cs
+++ Grupo3_TaskManager/GestorColas.cs
@@ -32,6 +32,36 @@ namespace Grupo3_TaskManager
             return ColaProcesos;
         }
 
+        public static void OrdenarPorPrioridad()
+        {
+            var listaOrdenada = ColaProcesos.OrderByDescending(p => p.Prioridad).ToList();
+            ActualizarCola(listaOrdenada);
+        }
 
+        public static void OrdenarPorTiempoCpu()
+        {
+            var listaOrdenada = ColaProcesos.OrderBy(p => p.TiempoCpu).ToList();
+            ActualizarCola(listaOrdenada);
+        }
+
+        public static void OrdenarPorTiempoLlegada()
+        {
+            var listaOrdenada = ColaProcesos.OrderBy(p => p.TiempoLlegada).ToList();
+            ActualizarCola(listaOrdenada);
+        }
+
+        public static Procesos ObtenerProcesoPorId(int id)
+        {
+            return ColaProcesos.FirstOrDefault(p => p.Id == id);
+        }
+
+        private static void ActualizarCola(List<Procesos> listaOrdenada)
+        {
+            ColaProcesos.Clear();
+            foreach (var proceso in listaOrdenada)
+            {
+                ColaProcesos.Enqueue(proceso);
+            }
+        }
     }
 }
